fd488a9d3330b5265c21f178e01b6b2ac03c722c
Leo <ustyugov@recursion.ru>
2022-06-28 01:25:09 +0400

    Add some comments

--- LX.GifTest/Program.cs
+++ LX.GifTest/Program.cs
@@ -9,9 +9,16 @@ namespace LX.GifTest
             App.OnRun += delegate
             {
                 var pictureBox = new PictureBox();
+
+                // Color(0, 51, 102, 255) - gif background color
                 pictureBox.Image = Image.LoadFromResource("*.cat.gif", new Color(0, 51, 102, 255));
                 pictureBox.AddToRoot(Alignment.Fill);
+
+                // play animation, you can create 
                 pictureBox.Play();
+
+                // You can create your own animation by simply adding frames to any image (Image.Frames)
+                // and setting the intervals for playing frames (Image.Delay)
 
                 var timerBox = new Label();
                 timerBox.Font = new Font("OpenSans-Bold.ttf", 200, 0, 0);
