700adf2181cae12b46694c732a7fffb833a75fa6
Leo <ustyugov@recursion.ru>
2022-06-28 01:07:32 +0400

    Add timer

--- LX.GifTest/Program.cs
+++ LX.GifTest/Program.cs
@@ -13,11 +13,16 @@ namespace LX.GifTest
                 pictureBox.AddToRoot(Alignment.Fill);
                 pictureBox.Play();
 
+                var timerBox = new Label();
+                timerBox.Font = new Font("OpenSans-Bold.ttf", 200, 0, 0);
+                timerBox.AddTo(pictureBox, Alignment.TopRight);
+
                 // change back color every 5 seconds
                 pictureBox.StartTimer().Tick += (object sender, Timer e) =>
                 {
                     var total = (int)e.TotalElapsed.TotalSeconds - ((int)e.TotalElapsed.TotalSeconds / 10) * 10;
                     pictureBox.Color = total < 5 ? Color.White : Color.Black;
+                    timerBox.Text = (total < 5 ? 5 - total : 10 - total).ToString();
                 };
             };
             App.Run();
